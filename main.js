f3bbe807e1ade85eebb394aec196b100d7070143
werty-lang <84279588+werty-lang@users.noreply.github.com>
2021-07-05 18:10:13 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,51 @@
+Webcam.set({
+    Height:300,
+    Width:350,
+    image_format: 'png',
+png_quality:90
+});
+
+camera =document.getElementById("camera")
+Webcam.attach('#camera');
+function take_snapshot(){
+        Webcam.snap(function(data_uri)
+        {
+            document.getElementById("result").innerHTML= '<img id="captured_image" src="'+data_uri+'"/>';
+
+        });
+}
+console.log('ml5 version:',ml5.version);
+classifier= ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/psDEmR7S7/model.json',modelLoaded);
+
+function modelLoaded(){
+    console.log("Model Loaded!")
+}
+
+
+function check(){
+    img=document.getElementById('captured_image');
+    classifier.classify(img, gotResult);
+}
+function gotResult(error, results){
+if(error) {
+    console.error(error);
+} else{
+    console.log(results);
+    document.getElementById("result_gesture_name").innerHTML = results[0].label;
+    prediction_1= results[0].label;
+
+    if(results[0].label =="happy")
+    {
+        document.getElementById("update_gesture").innerHTML = "&#128512;";
+    }
+    if(results[0].label =="sad")
+    {
+        document.getElementById("update_gesture").innerHTML = "&#128532;";
+    }
+    if(results[0].label =="angry")
+    {
+        document.getElementById("update_gesture").innerHTML = "&#128548; ";
+    }
+    
+}
+}
